ccbc1f4952e9c587d0d6ea7ce445eb7c1f845d6a
Bruce <bruce@storyprotocol.xyz>
2023-12-08 19:27:17 +1100

    Add support for env block

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,18 +13,22 @@
 setup() {
 	local workdir=${1:-.}; shift
 	local envfile=${1:-.env}; shift
+	local envblock=$1; shift
 	if [ ! -d "$workdir" ]; then
 		echo "--> Work dir "$workdir" does not exist. Exiting..." >&2
 		exit 0
 	fi
 	cd "$workdir"
 
+	local exportscript=$(mktemp)
 	if [ -f "$envfile" ]; then
-		local exportscript=$(mktemp)
-		cat "$envfile" | grep -v "^\s*#.*" | sed 's:^:export :g' > "$exportscript"
-		source "$exportscript"
-		rm -f "$exportscript"
+		cat "$envfile" | grep -v "^\s*$" | grep -v "^\s*#.*" | sed 's:^:export :g' >> "$exportscript"
 	fi
+	if [ -n "$envblock" ]; then
+		echo "$envblock" | grep -v "^\s*$" | grep -v "^\s*#.*" | sed 's:^:export :g' >> "$exportscript"
+	fi
+	source "$exportscript"
+	rm -f "$exportscript"
 
 	create_gitconfig
 	if [ -n "$TF_CLI_ARGS_init" ]; then
@@ -78,8 +82,9 @@ parse_and_run_command() {
 main() {
 	local workdir=$1; shift
 	local envfile=$1; shift
+	local envblock=$1; shift
 	local command=$1; shift
-	setup "$workdir" "$envfile"
+	setup "$workdir" "$envfile" "$envblock"
 	echo "--> Executing '$command'..." >&2
 	$command
 	ecode=$?
